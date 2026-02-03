b7e3e45b8796a42971a63d3b32ac9e9c0d6fac03
Nvm-Win <marckantonio@gmail.com>
2026-02-03 18:50:07 -0500

    Entorno v6 fix bugs

--- prog/hypr/scripts/Switch_Wallpaper.sh
+++ prog/hypr/scripts/Switch_Wallpaper.sh
@@ -4,7 +4,10 @@
 # paplay "$HOME/.config/hypr/assests/sounds/wallpaper_change.mp3"
 image_path=$(find ~/.wallpapers/ -type f | shuf -n1)
 swww img $image_path --transition-type wipe --transition-fps 60
-wal -i $image_path -n ; hyprctl reload ; pkill -f "Kitty_Cava"
+wal -i $image_path -n
+pkill -f "Kitty_Cava"
+# Reload Waybar to pick up new colors (SIGUSR2 reloads CSS)
+killall -SIGUSR2 waybar
 kitty +kitten panel --edge=none --columns=2660px --lines=1500px --config "$HOME/.config/hypr/kittyconfigbg.conf" --margin-left=0 --margin-bottom=50 --name "Kitty_Cava" cava
 #bash $HOME/.cache/wal/tclock.sh
 gsettings set org.gnome.desktop.interface gtk-theme FlatColor && gsettings set org.gnome.desktop.interface gtk-theme pywall-dynamic
